953b4a3a73f23288b52f4b2bbb79f6885c4da9b6
jao006 <joaomelquesedequecalaca95@gmail.com>
2024-12-04 13:39:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('natal-word').addEventListener('click', () => {
+      const message = document.getElementById('message');
+      message.classList.remove('hidden');
+    });
